c44682b2592568891535231c572c84a2316f04b9
chrisChatzi <christos.chatziioannidis@gmail.com>
2016-09-02 18:06:34 +0200

    - updated README.md - no babel es6 needed

--- README.md
+++ README.md
@@ -18,7 +18,105 @@ const MyComponent = ( { data } ) => (
 MyComponent.propTypes = {
 	data: PropTypes.object.isRequired
 }
+```Simple chart for react-redux developing.
+
+Just call the component:
+
+```javascript
+import Chart from 'react-redux-chart'
+
+const MyComponent = ( { data } ) => (
+	<div className="myComponent">
+		<div className="chart">
+			<Chart data={data} />
+		</div>
+	</div>
+)
+
+MyComponent.propTypes = {
+	data: PropTypes.object.isRequired
+}
 ```
+
+####**Data structure**
+
+Data passed to component has the following structure:
+
+```javascript
+let data = {
+	//title
+	title : "react-redux-chart",
+	//x-axis labels*
+	xAxisValues : ["Jan", "Feb", "Mar"],
+	//number of y-axis steps
+	yAxisPoints : 4,
+	//show grid		
+	showGrid : true,
+	//show circle points
+	showPoints : true,
+	//show circle points
+	showOuterPoints : true,
+	//show lines between points
+	showLines : true,
+	//show chart title
+	showTitle : true,
+	//show chart info
+	showInfo : true,
+	//hover on/off
+	showHover : true,
+	//allow filtering of lines
+	allowFilters : true,
+	//values of chart*
+	values : {
+		'dataset1' : [1, 2, 2],
+		'dataset2' : [2, 5, -5]
+	},
+	//chart style
+	chartStyle : {
+		gridColor : "#ccc",
+		xAxisTextColor : "#242428",
+		yAxisTextColor : "#242428",
+		xAxisTextSize : "10",
+		yAxisTextSize : "10",
+		colors : ["#e74c3c", "#3498db", "#2ecc71", "#e67e22", "#9b59b6"]
+	}
+}
+```
+####**Default values and restrictions**
+
+All the keys have the default values as shown above except:
+
+**xAxisValues**: These are the values of the x-axis. It has to be an array.
+**values**: These are the data of the chart. It has to be an object of arrays.
+
+**xAxisValues** array and each dataset array of the **values** object must have the same length
+
+**chartStyle.colors** should have one color for each dataset of the chart. If not the default would be #ccc for all the additionals
+
+####**Mandatory fields**
+
+Only 2 fields are mandatory
+
+**xAxisValues** and **values**
+
+```javascript
+	let data = {
+		xAxisValues : ["Jan", "Feb", "Mar"],
+		values : {
+			'my dataset 1' : [1, 2, 2],
+			'my dataset 2' : [2, 5, -5],
+			'my dataset 3' : [3, 2, 21]
+		}
+	}
+```
+
+####**Additional info**
+
+The chart takes the full width and height of the div that it will be placed in.
+It uses 3 divs:
+- The title div at the top (can be hidden) | (40px height)
+- The chart div
+- The info div at the bottom (can be hidden) | (40px height)
 
 ####**Data structure**
 
--- dist/bundle.js
+++ dist/bundle.js
@@ -1,17 +1,9 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-
-var _reactRedux = require('react-redux');
-
-var _ChartView = require('./ChartView');
-
-var _ChartView2 = _interopRequireDefault(_ChartView);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var ReactRedux = require('react-redux');
+var ChartView = require('./ChartView');
+// import ChartView from './ChartView'
 
 var parsed = {};
 
@@ -233,22 +225,15 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch, ownProps) {
 	};
 };
 
-var Chart = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(_ChartView2.default);
+var Chart = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(ChartView);
 
-exports.default = Chart;
+// export default Chart
+module.exports = Chart;
 
 },{"./ChartView":2,"react-redux":7}],2:[function(require,module,exports){
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var React = require('react');
 
 var Chart = function Chart(_ref) {
 	var data = _ref.data;
@@ -257,216 +242,105 @@ var Chart = function Chart(_ref) {
 	var clickInfo = _ref.clickInfo;
 	var hoverInfo = _ref.hoverInfo;
 	var houtInfo = _ref.houtInfo;
-	return _react2.default.createElement(
-		'div',
-		{ className: 'chart animated fadeIn',
-			style: { width: '100%', height: '100%', overflow: 'auto', overflowY: 'hidden', cursor: 'default' } },
-		data.showTitle ? _react2.default.createElement(
-			'div',
-			{ className: 'chart-title', style: { height: '40px', lineHeight: '40px' } },
-			data.title
-		) : "",
-		_react2.default.createElement(
-			'div',
-			{ className: 'chart-main' },
-			_react2.default.createElement(
-				'svg',
-				{ className: 'chart-svg', stroke: '#ccc', style: { width: '100%', height: '100%' } },
-				'/* x-axis grid lines */',
-				data.showGrid ? data.xAxisPoints.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: data.chartStyle.gridColor },
-						_react2.default.createElement('line', { x1: item + "%", y1: '90%', x2: item + "%", y2: '5%' })
-					);
-				}) : "",
-				'/* y-axis grid lines */',
-				data.showGrid ? data.yAxisPoints.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: data.chartStyle.gridColor },
-						_react2.default.createElement('line', { x1: '10%', y1: item + "%", x2: '95%', y2: item + "%" })
-					);
-				}) : "",
-				'/* axes lines */',
-				_react2.default.createElement(
-					'g',
-					{ stroke: data.chartStyle.gridColor },
-					_react2.default.createElement('line', { x1: '10%', y1: '90%', x2: '10%', y2: '5%' }),
-					_react2.default.createElement('line', { x1: '10%', y1: '90.1%', x2: '95%', y2: '90.1%' })
-				),
-				'/* y-axis text */',
-				data.values.yAxisText.map(function (yAxis, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: data.chartStyle.yAxisTextColor,
-							fontSize: data.chartStyle.yAxisTextSize },
-						_react2.default.createElement(
-							'text',
-							{ alignmentBaseline: 'middle', textAnchor: 'end',
-								x: '8%', y: data.yAxisPoints[i] + "%" },
-							yAxis
-						)
-					);
-				}),
-				'/* x-axis text */',
-				data.values.xAxisText.map(function (yAxis, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: data.chartStyle.yAxisTextColor,
-							fontSize: data.chartStyle.xAxisTextSize },
-						_react2.default.createElement(
-							'text',
-							{ alignmentBaseline: 'before-edge', textAnchor: 'middle',
-								y: '92%', x: data.xAxisPoints[i] + "%" },
-							yAxis
-						)
-					);
-				}),
-				'//lines',
-				data.showLines ? data.values.colors.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: item, fill: item },
-						data.values.lines[i].map(function (line, xi) {
-							return _react2.default.createElement('line', { key: xi, className: data.keys[i] + "-lines", strokeLinecap: 'round',
-								x1: data.values.lines[i][xi]['x1'] + "%",
-								y1: data.values.lines[i][xi]['y1'] + "%",
-								x2: data.values.lines[i][xi]['x2'] + "%",
-								y2: data.values.lines[i][xi]['y2'] + "%" });
-						})
-					);
-				}) : "",
-				'//points //inner circle',
-				data.showPoints ? data.values.colors.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: item, fill: item },
-						data.values.coordX[i].map(function (x, xi) {
-							return _react2.default.createElement(
-								'circle',
-								{ key: xi, r: '3',
-									className: data.keys[i] + "-points",
-									value: data.values.real[i][xi],
-									name: data.values.xAxisText[xi],
-									title: data.keys[i],
-									id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
-									cx: data.values.coordX[i][xi] + "%", cy: data.values.coordY[i][xi] + "%" },
-								_react2.default.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
-									from: '90%', to: data.values.coordY[i][xi] + "%" })
-							);
-						})
-					);
-				}) : "",
-				'//outer circle',
-				data.showPoints && data.showOuterPoints ? data.values.colors.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: item, fill: item },
-						data.values.coordX[i].map(function (x, xi) {
-							return _react2.default.createElement(
-								'circle',
-								{ key: xi, r: '5',
-									fill: 'none',
-									className: data.keys[i] + "-points",
-									value: data.values.real[i][xi],
-									name: data.values.xAxisText[xi],
-									title: data.keys[i],
-									id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
-									cx: data.values.coordX[i][xi] + "%",
-									cy: data.values.coordY[i][xi] + "%" },
-								_react2.default.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
-									from: '90%', to: data.values.coordY[i][xi] + "%" })
-							);
-						})
-					);
-				}) : "",
-				'//hover rect',
-				data.showPoints ? data.values.colors.map(function (item, i) {
-					return _react2.default.createElement(
-						'g',
-						{ key: i, stroke: item, fill: item },
-						data.values.coordX[i].map(function (x, xi) {
-							return _react2.default.createElement(
-								'rect',
-								{ key: xi,
-									fillOpacity: '0',
-									strokeOpacity: '0',
-									className: data.keys[i] + "-points",
-									value: data.values.real[i][xi],
-									name: data.values.xAxisText[xi],
-									title: data.keys[i],
-									id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
-									onMouseOver: function onMouseOver(e) {
-										return data.showHover ? hover(e) : "";
-									},
-									onMouseOut: function onMouseOut(e) {
-										return data.showHover ? hout(e) : "";
-									},
-									x: data.values.coordX[i][xi] - 2 + "%",
-									y: data.values.coordY[i][xi] - 4 + "%",
-									width: '4%', height: '8%' },
-								_react2.default.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
-									from: '90%', to: data.values.coordY[i][xi] + "%" })
-							);
-						})
-					);
-				}) : ""
-			)
-		),
-		data.showInfo ? _react2.default.createElement(
-			'div',
-			{ className: 'chart-info', style: { height: '40px', lineHeight: '40px' } },
-			data.keys.map(function (item, i) {
-				return _react2.default.createElement(
-					'div',
-					{ key: i, style: { float: 'left', margin: '0 5px', padding: '0 5px' },
-						onMouseOver: function onMouseOver(e) {
-							return data.allowFilters ? hoverInfo(e) : "";
-						},
-						onMouseOut: function onMouseOut(e) {
-							return data.allowFilters ? houtInfo(e) : "";
-						},
-						onClick: function onClick(e) {
-							return data.allowFilters ? clickInfo(e) : "";
-						},
-						name: '0', id: item, className: 'chart-info-item' },
-					_react2.default.createElement(
-						'div',
-						{ className: 'chart-info-item-icon', style: { background: data.values.colors[i],
-								float: 'left', width: '16px', height: '16px', margin: '12px 6px' } },
-						' '
-					),
-					_react2.default.createElement(
-						'div',
-						{ className: 'chart-info-item-text', style: { float: 'left' } },
-						item
-					)
-				);
-			})
-		) : "",
-		_react2.default.createElement('div', { id: 'chartHoverIndex', className: 'chart-hover-index',
-			style: { display: 'none', position: 'absolute',
-				borderTop: '10px solid transparent',
-				borderBottom: '10px solid transparent',
-				borderRight: '10px solid rgba(0,0,0,0.75)',
-				top: '0', left: '0', width: '0', height: '0', zIndex: '1' } }),
-		_react2.default.createElement('div', { id: 'chartHover', className: 'chart-hover', style: { display: 'none', background: 'rgba(0, 0, 0, 0.75)',
-				color: '#ddd', position: 'absolute', top: '0', left: '0', width: 'auto', height: 'auto',
-				padding: '5px 10px', borderRadius: '4px', zIndex: '2' } })
-	);
+	return React.createElement('div', { className: 'chart animated fadeIn',
+		style: { width: '100%', height: '100%', overflow: 'auto', overflowY: 'hidden', cursor: 'default' } }, data.showTitle ? React.createElement('div', { className: 'chart-title', style: { height: '40px', lineHeight: '40px' } }, data.title) : "", React.createElement('div', { className: 'chart-main' }, React.createElement('svg', { className: 'chart-svg', stroke: '#ccc', style: { width: '100%', height: '100%' } }, '/* x-axis grid lines */', data.showGrid ? data.xAxisPoints.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: data.chartStyle.gridColor }, React.createElement('line', { x1: item + "%", y1: '90%', x2: item + "%", y2: '5%' }));
+	}) : "", '/* y-axis grid lines */', data.showGrid ? data.yAxisPoints.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: data.chartStyle.gridColor }, React.createElement('line', { x1: '10%', y1: item + "%", x2: '95%', y2: item + "%" }));
+	}) : "", '/* axes lines */', React.createElement('g', { stroke: data.chartStyle.gridColor }, React.createElement('line', { x1: '10%', y1: '90%', x2: '10%', y2: '5%' }), React.createElement('line', { x1: '10%', y1: '90.1%', x2: '95%', y2: '90.1%' })), '/* y-axis text */', data.values.yAxisText.map(function (yAxis, i) {
+		return React.createElement('g', { key: i, stroke: data.chartStyle.yAxisTextColor,
+			fontSize: data.chartStyle.yAxisTextSize }, React.createElement('text', { alignmentBaseline: 'middle', textAnchor: 'end',
+			x: '8%', y: data.yAxisPoints[i] + "%" }, yAxis));
+	}), '/* x-axis text */', data.values.xAxisText.map(function (yAxis, i) {
+		return React.createElement('g', { key: i, stroke: data.chartStyle.yAxisTextColor,
+			fontSize: data.chartStyle.xAxisTextSize }, React.createElement('text', { alignmentBaseline: 'before-edge', textAnchor: 'middle',
+			y: '92%', x: data.xAxisPoints[i] + "%" }, yAxis));
+	}), '//lines', data.showLines ? data.values.colors.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: item, fill: item }, data.values.lines[i].map(function (line, xi) {
+			return React.createElement('line', { key: xi, className: data.keys[i] + "-lines", strokeLinecap: 'round',
+				x1: data.values.lines[i][xi]['x1'] + "%",
+				y1: data.values.lines[i][xi]['y1'] + "%",
+				x2: data.values.lines[i][xi]['x2'] + "%",
+				y2: data.values.lines[i][xi]['y2'] + "%" });
+		}));
+	}) : "", '//points //inner circle', data.showPoints ? data.values.colors.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: item, fill: item }, data.values.coordX[i].map(function (x, xi) {
+			return React.createElement('circle', { key: xi, r: '3',
+				className: data.keys[i] + "-points",
+				value: data.values.real[i][xi],
+				name: data.values.xAxisText[xi],
+				title: data.keys[i],
+				id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
+				cx: data.values.coordX[i][xi] + "%", cy: data.values.coordY[i][xi] + "%" }, React.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
+				from: '90%', to: data.values.coordY[i][xi] + "%" }));
+		}));
+	}) : "", '//outer circle', data.showPoints && data.showOuterPoints ? data.values.colors.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: item, fill: item }, data.values.coordX[i].map(function (x, xi) {
+			return React.createElement('circle', { key: xi, r: '5',
+				fill: 'none',
+				className: data.keys[i] + "-points",
+				value: data.values.real[i][xi],
+				name: data.values.xAxisText[xi],
+				title: data.keys[i],
+				id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
+				cx: data.values.coordX[i][xi] + "%",
+				cy: data.values.coordY[i][xi] + "%" }, React.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
+				from: '90%', to: data.values.coordY[i][xi] + "%" }));
+		}));
+	}) : "", '//hover rect', data.showPoints ? data.values.colors.map(function (item, i) {
+		return React.createElement('g', { key: i, stroke: item, fill: item }, data.values.coordX[i].map(function (x, xi) {
+			return React.createElement('rect', { key: xi,
+				fillOpacity: '0',
+				strokeOpacity: '0',
+				className: data.keys[i] + "-points",
+				value: data.values.real[i][xi],
+				name: data.values.xAxisText[xi],
+				title: data.keys[i],
+				id: "point" + xi + "-" + data.keys[i] + "-" + data.values.real[i][xi],
+				onMouseOver: function onMouseOver(e) {
+					return data.showHover ? hover(e) : "";
+				},
+				onMouseOut: function onMouseOut(e) {
+					return data.showHover ? hout(e) : "";
+				},
+				x: data.values.coordX[i][xi] - 2 + "%",
+				y: data.values.coordY[i][xi] - 4 + "%",
+				width: '4%', height: '8%' }, React.createElement('animate', { attributeType: 'CSS', attributeName: 'cy', dur: '1s',
+				from: '90%', to: data.values.coordY[i][xi] + "%" }));
+		}));
+	}) : "")), data.showInfo ? React.createElement('div', { className: 'chart-info', style: { height: '40px', lineHeight: '40px' } }, data.keys.map(function (item, i) {
+		return React.createElement('div', { key: i, style: { float: 'left', margin: '0 5px', padding: '0 5px' },
+			onMouseOver: function onMouseOver(e) {
+				return data.allowFilters ? hoverInfo(e) : "";
+			},
+			onMouseOut: function onMouseOut(e) {
+				return data.allowFilters ? houtInfo(e) : "";
+			},
+			onClick: function onClick(e) {
+				return data.allowFilters ? clickInfo(e) : "";
+			},
+			name: '0', id: item, className: 'chart-info-item' }, React.createElement('div', { className: 'chart-info-item-icon', style: { background: data.values.colors[i],
+				float: 'left', width: '16px', height: '16px', margin: '12px 6px' } }, ' '), React.createElement('div', { className: 'chart-info-item-text', style: { float: 'left' } }, item));
+	})) : "", React.createElement('div', { id: 'chartHoverIndex', className: 'chart-hover-index',
+		style: { display: 'none', position: 'absolute',
+			borderTop: '10px solid transparent',
+			borderBottom: '10px solid transparent',
+			borderRight: '10px solid rgba(0,0,0,0.75)',
+			top: '0', left: '0', width: '0', height: '0', zIndex: '1' } }), React.createElement('div', { id: 'chartHover', className: 'chart-hover', style: { display: 'none', background: 'rgba(0, 0, 0, 0.75)',
+			color: '#ddd', position: 'absolute', top: '0', left: '0', width: 'auto', height: 'auto',
+			padding: '5px 10px', borderRadius: '4px', zIndex: '2' } }));
 };
 
 Chart.propTypes = {
-	data: _react.PropTypes.object.isRequired,
-	hover: _react.PropTypes.func.isRequired,
-	hout: _react.PropTypes.func.isRequired,
-	clickInfo: _react.PropTypes.func.isRequired,
-	hoverInfo: _react.PropTypes.func.isRequired,
-	houtInfo: _react.PropTypes.func.isRequired
+	data: React.PropTypes.object.isRequired,
+	hover: React.PropTypes.func.isRequired,
+	hout: React.PropTypes.func.isRequired,
+	clickInfo: React.PropTypes.func.isRequired,
+	hoverInfo: React.PropTypes.func.isRequired,
+	houtInfo: React.PropTypes.func.isRequired
 };
 
-exports.default = Chart;
+module.exports = Chart;
 
 },{"react":250}],3:[function(require,module,exports){
 // shim for using process in browser
